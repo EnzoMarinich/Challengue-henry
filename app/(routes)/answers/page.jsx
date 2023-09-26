5a9920258f222171b01ba31bf7a841314ed98dee
EnzoMarinich <marinichenzo@gmail.com>
2023-09-26 01:12:29 -0300

    mmm

--- app/(routes)/answers/page.jsx
+++ app/(routes)/answers/page.jsx
@@ -1,36 +1,36 @@
-"use client"
-import React, {useEffect, useState} from 'react'
+
+import React from 'react'
 import Form from '@/app/components/Form'
 import axios from 'axios'
 import { Button } from '@/app/components/Button'
 
-export default function Page() {
+export default async function Page() {
     
 
-  // const rstas = await axios("https://challengue-henry-dv5kt6186-enzomarinich.vercel.app/app/api/answer")
+  const rstas = await axios("https://challengue-henry-dv5kt6186-enzomarinich.vercel.app/api/answer")
 
   
-  // const data = (await (axios("https://challengue-henry-dv5kt6186-enzomarinich.vercel.app/app/api/form"))).data.items
+  const data = (await (axios("https://challengue-henry-dv5kt6186-enzomarinich.vercel.app/api/form"))).data.items
 
-  const [data, setData] = useState(null);
-  const [rstas, setRstas] = useState(null);
+  // const [data, setData] = useState(null);
+  // const [rstas, setRstas] = useState(null);
 
-  useEffect(() => {
-    async function fetchData() {
-      const dataResponse = await axios("https://challengue-henry-dv5kt6186-enzomarinich.vercel.app/app/api/form");
-      const rstasResponse = await axios("https://challengue-henry-dv5kt6186-enzomarinich.vercel.app/app/api/answer");
-      setData(dataResponse.data.items);
-      setRstas(rstasResponse.data.data);
-    }
-    fetchData();
-  }, []);
-  if(!data || !rstas ){
-    return (
-      <div className='relative h-full flex flex-col items-center gap-9 p-20'>
-        <h1 className='text-3xl text-orange-500 font-semibold'>Cargando...</h1>
-      </div>
-    )
-  }
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const dataResponse = await axios("https://challengue-henry-dv5kt6186-enzomarinich.vercel.app/app/api/form");
+  //     const rstasResponse = await axios("https://challengue-henry-dv5kt6186-enzomarinich.vercel.app/app/api/answer");
+  //     setData(dataResponse.data.items);
+  //     setRstas(rstasResponse.data.data);
+  //   }
+  //   fetchData();
+  // }, []);
+  // if(!data || !rstas ){
+  //   return (
+  //     <div className='relative h-full flex flex-col items-center gap-9 p-20'>
+  //       <h1 className='text-3xl text-orange-500 font-semibold'>Cargando...</h1>
+  //     </div>
+  //   )
+  // }
 
 
   return (
