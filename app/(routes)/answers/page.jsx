a6da546f073141b0474ee1a649f1af3f89ff4de6
EnzoMarinich <marinichenzo@gmail.com>
2023-09-26 00:18:20 -0300

    prueba 222

--- app/(routes)/answers/page.jsx
+++ app/(routes)/answers/page.jsx
@@ -2,16 +2,15 @@
 import React from 'react'
 import Form from '@/app/components/Form'
 import axios from 'axios'
-import fondo from "@/public/questions.jpg"
 import { Button } from '@/app/components/Button'
 
 export default async function Page() {
     
 
-  const rstas = await axios("https://challengue-henry-dv5kt6186-enzomarinich.vercel.app/api/answer")
+  const rstas = await axios("https://challengue-henry-dv5kt6186-enzomarinich.vercel.app/app/api/answer")
 
   
-  const data = (await (axios("https://challengue-henry-dv5kt6186-enzomarinich.vercel.app/api/form"))).data.items
+  const data = (await (axios("https://challengue-henry-dv5kt6186-enzomarinich.vercel.app/app/api/form"))).data.items
 
   if(!data || !rstas ){
     return (
@@ -21,7 +20,7 @@ export default async function Page() {
     )
   }
 
-  
+
   return (
     <div className='relative h-full flex flex-col items-center gap-9 p-20'>
         <Button className="absolute rounded-md top-7 left-10 bg-black font-semibold text-orange-400 px-16 py-1 hover:bg-orange-400 hover:text-black transition-all" route="/" label="Atras" />
