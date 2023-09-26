119f8c8c846c100303323ca1368d352dcc611326
EnzoMarinich <marinichenzo@gmail.com>
2023-09-26 00:23:52 -0300

    prube2 323239

--- app/(routes)/answers/page.jsx
+++ app/(routes)/answers/page.jsx
@@ -12,20 +12,12 @@ export default async function Page() {
   
   const data = (await (axios("https://challengue-henry-dv5kt6186-enzomarinich.vercel.app/app/api/form"))).data.items
 
-  if(!data || !rstas ){
-    return (
-      <div className='relative h-full flex flex-col items-center gap-9 p-20'>
-        <h1 className='text-3xl text-orange-500 font-semibold'>Cargando...</h1>
-      </div>
-    )
-  }
-
 
   return (
     <div className='relative h-full flex flex-col items-center gap-9 p-20'>
         <Button className="absolute rounded-md top-7 left-10 bg-black font-semibold text-orange-400 px-16 py-1 hover:bg-orange-400 hover:text-black transition-all" route="/" label="Atras" />
         <h1 className='text-3xl text-orange-500 font-semibold'>Respuestas</h1>
-        {rstas.data.data.map(e=> <Form key={e.name} data={data} rsta={e} edit={true}/>)}
+        {rstas?.data?.data?.map(e=> <Form key={e.name} data={data} rsta={e} edit={true}/>)}
     </div>
   )
 }
